7d8e721e12ee88883087a82ba0992491ba1f59eb
danilo.costa <danilo.costa@olist.com>
2022-02-13 17:32:33 -0300

    feat(db): url table has been added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_182834) do
+ActiveRecord::Schema.define(version: 2022_02_13_202133) do
+
+  create_table "urls", force: :cascade do |t|
+    t.string "default_url"
+    t.string "short_url"
+    t.integer "numbers_access"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_urls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2022_02_13_182834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "urls", "users"
 end

ccba1ebd2b72d0d9e5306f345017ae4cd18fb820
danilo.costa <danilo.costa@olist.com>
2022-02-14 17:09:42 -0300

    feat(routes): url get has been added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   root to: "home#index"
-
-  devise_for :users
+  post "shorten" => "home#shorten"
   
+  devise_for :users
+  get 'shortener/:id' => "shortener/shortened_urls#show"
+
 end

bf21a29d0e22537d2851a26ab702d797b7c487e6
danilo.costa <danilo.costa@olist.com>
2022-02-14 15:36:04 -0300

    refactor(migrate): url migrate has been removed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   
-  root to: "urls#new"
+  root to: "home#index"
 
   devise_for :users
-  resources :urls
-
-  get 'home/index'
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_202133) do
-
-  create_table "urls", force: :cascade do |t|
-    t.string "default_url"
-    t.string "short_url"
-    t.integer "numbers_access"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_urls_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2022_02_13_182834) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,5 +34,4 @@ ActiveRecord::Schema.define(version: 2022_02_13_202133) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "urls", "users"
 end

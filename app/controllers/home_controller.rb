b61817800fdc8139d2c18e416e46ea8cd966c07d
danilo.costa <danilo.costa@olist.com>
2022-02-15 17:35:06 -0300

    feat(home_controller): shorten func has been added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,21 @@ class HomeController < ApplicationController
   end
 
   def shorten
+    respond_to do |format|
+      required = :input_url
+      form_complete = true
+      if params.has_key? required and not params[required].blank?
+          
+          Shortener::ShortenedUrl.generate(params[required], owner: current_user)
+
+          format.html { redirect_to root_path, notice: "URL shortened successfully." }
+          format.json { head :no_content }
+        else
+          form_complete = false
+        end
+    end
   end
 
-  def index
-  end
+  # def index
+  # end
 end

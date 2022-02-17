1bd9219580619e6c09e59639783d51486cf02e3e
danilo.costa <danilo.costa@olist.com>
2022-02-16 22:05:21 -0300

    refactor(links): shortener url is show in table.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,19 @@ class HomeController < ApplicationController
       required = :input_url
       form_complete = true
       if params.has_key? required and not params[required].blank?
-          
-          Shortener::ShortenedUrl.generate(params[required], owner: current_user)
-          flash.now[:alert] = "URL shortened successfully."
-          format.html { redirect_to root_path, notice: "URL shortened successfully." }
-          format.json { head :no_content }
-        else
-          form_complete = false
-        end
+        shortened_url = Shortener::ShortenedUrl.generate(params[required], owner: current_user)
+        flash.now[:notice] = "URL shortened successfully."
+        format.html { redirect_to root_path }
+        format.json { head :no_content }
+      else
+        form_complete = false
+      end
     end
   end
-
+  
   def links
+    # byebug
+    @base_url = "#{request.base_url}/shortener/"
     @links = current_user.shortened_urls
   end
 

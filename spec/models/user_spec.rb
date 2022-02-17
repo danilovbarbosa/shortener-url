6f37fe8a408e7a142e5a69719b2b9bc4410fdea3
danilo.costa <danilo.costa@olist.com>
2022-02-17 17:20:24 -0300

    test(factories): user factories has been added.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,34 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "is valid with valid attributes" do
     password_fake = Faker::Internet.password(min_length: 8)
+    # user = User.new(name: Faker::Name.name, username: Faker::Internet.username, email: Faker::Internet.email, password: password_fake, password_confirmation: password_fake)
+    user = build(:user)
     expect(
-      User.new(name: Faker::Name.name, username: Faker::Internet.username, email: Faker::Internet.email, password: password_fake, password_confirmation: password_fake)
+      user
     ).to be_valid
   end
 
-  # it "is not valid without a title" do
-  #   user = User.new(username: nil)
-  #   expect(user).to_not be_valid
-  # end
+  it "is not valid without a name" do
+    password_fake = Faker::Internet.password(min_length: 8)
+    user = User.new(name: nil, username: Faker::Internet.username, email: Faker::Internet.email, password: password_fake, password_confirmation: password_fake)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a username" do
+    password_fake = Faker::Internet.password(min_length: 8)
+    user = User.new(name: Faker::Name.name, username: nil, email: Faker::Internet.email, password: password_fake, password_confirmation: password_fake)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a email" do
+    password_fake = Faker::Internet.password(min_length: 8)
+    user = User.new(name: Faker::Name.name, username: Faker::Internet.username, email: nil, password: password_fake, password_confirmation: password_fake)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    password_fake = Faker::Internet.password(min_length: 8)
+    user = User.new(name: Faker::Name.name, username: Faker::Internet.username, email: Faker::Internet.email, password: nil, password_confirmation: password_fake)
+    expect(user).to_not be_valid
+  end
 end

fddcad126a1e0aa3ce5d6d65e1231925febdba1f
danilo.costa <danilo.costa@olist.com>
2022-02-17 21:07:27 -0300

    test(controller_macros): devise configs to rspec has been added.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,11 @@ require 'rspec/rails'
 require 'support/factory_bot'
 require 'database_cleaner/active_record'
 
+# Add these after require 'rspec/rails'
+require 'devise'
+require 'support/controller_macros'
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -76,4 +81,9 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  # Add these
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include FactoryBot::Syntax::Methods
+  config.extend ControllerMacros, :type => :controller
 end
